1b7c1c16e6f31d5cd0247739e0778017ceef11d4
Xon <tazipper1@hotmail.com>
2015-08-21 09:08:49 +0800

    Initial work for capturing timings  for controller action & view rendering

--- upload/library/SV/Telemetry/Globals.php
+++ upload/library/SV/Telemetry/Globals.php
@@ -4,6 +4,11 @@
 // relying on the consumer being loaded correctly by the dynamic class autoloader
 class SV_Telemetry_Globals
 {
+    public static function addTiming($type, $id, $contentIds, $starttime, $endtime)
+    {
+        $data = json_encode(array($type, $id, $contentIds, $starttime, $endtime));
+        XenForo_Error::debug($type .' '. $id . ' '.var_export($contentIds, true) . ' ' .($endtime - $starttime). 's');
+    }
 
     private function __construct() {}
 }
--- upload/library/SV/Telemetry/Listener.php
+++ upload/library/SV/Telemetry/Listener.php
@@ -8,4 +8,30 @@ class SV_Telemetry_Listener
     {
         $extend[] = self::AddonNameSpace.$class;
     }
+
+    public static $lastControllerKey = null;
+    public static $lastControllerIds = null;
+
+    public static function pre_dispatch(XenForo_Controller $controller, $action, $controllerName)
+    {
+        $controller->telemetry_starttime = microtime(true);
+    }
+
+    public static function post_dispatch(XenForo_Controller $controller, $controllerResponse, $controllerName, $action)
+    {
+        self::$lastControllerKey = $controllerName. '::'. $action;
+        self::$lastControllerIds = array();
+        SV_Telemetry_Globals::addTiming('controller', self::$lastControllerKey, $controller->telemetry_starttime, microtime(true));
+    }
+
+
+    public static function pre_view(XenForo_FrontController $fc, XenForo_ControllerResponse_Abstract &$controllerResponse, XenForo_ViewRenderer_Abstract &$viewRenderer, array &$containerParams)
+    {
+        $fc->telemetry_starttime = microtime(true);
+    }
+
+    public static function post_view(XenForo_FrontController $fc, &$output)
+    {
+        SV_Telemetry_Globals::addTiming('view', self::$lastControllerKey, self::$lastControllerIds, $fc->telemetry_starttime, microtime(true));
+    }
 }
