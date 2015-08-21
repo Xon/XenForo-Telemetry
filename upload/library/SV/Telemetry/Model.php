605693036602e081a06abde78b2f55afc00629a7
Xon <tazipper1@hotmail.com>
2015-08-21 18:16:35 +0800

    Tweak loader for Fluent

--- upload/library/SV/Telemetry/Model.php
+++ upload/library/SV/Telemetry/Model.php
@@ -8,24 +8,24 @@ class SV_Telemetry_Model
 
     public static function setupLogger($host = "unix:///var/run/td-agent/td-agent.sock", $port = 0)
     {
-        if ($this->logger !== null)
+        if (self::$logger !== null)
         {
             return;
         }
-        require_once __DIR__.'/../Fluent/Autoloader.php';
+        require_once __DIR__.'/Fluent/Autoloader.php';
         Fluent\Autoloader::register();
-        $this->logger = new \Fluent\Logger\FluentLogger($host, $port);
+        self::$logger = new \Fluent\Logger\FluentLogger($host, $port);
         Fluent\Autoloader::unregister();
     }
 
     public static function postTiming($key, $startTime, $endTime, $contentType = null, $contentId = null)
     {
-        if ($this->logger === null)
+        if (self::$logger === null)
         {
-            self::setupLogger();
+            self::setupLogger(XenForo_Application::getOptions()->sv_telemetry_fluent_connection);
         }
 
-        $this->logger->post($key, array('_type' => $contentType, "_id" => $contentId, "start"=>$startTime, "end"=>$endTime));
+        self::$logger->post($key, array('_type' => $contentType, "_id" => $contentId, "start"=>$startTime, "end"=>$endTime));
     }
 
     private function __construct() {}
