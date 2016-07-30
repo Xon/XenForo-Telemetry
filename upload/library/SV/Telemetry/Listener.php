f5b287a9f68eea2317b18aaecfe3e4da92b16992
Xon <tazipper1@hotmail.com>
2016-07-30 11:02:07 +0800

    Log controller as a tag, not the action

--- upload/library/SV/Telemetry/Listener.php
+++ upload/library/SV/Telemetry/Listener.php
@@ -14,13 +14,13 @@ class SV_Telemetry_Listener
 
     public static function pre_dispatch(XenForo_Controller $controller, $action, $controllerName)
     {
-        self::$lastControllerKey = $controllerName. '.'. $action;
+        self::$lastControllerKey = $controllerName;//. '.'. $action;
         $controller->telemetry_starttime = microtime(true);
     }
 
     public static function post_dispatch(XenForo_Controller $controller, $controllerResponse, $controllerName, $action)
     {
-        self::$lastControllerKey = $controllerName. '.'. $action;
+        self::$lastControllerKey = $controllerName;//. '.'. $action;
         if (!empty($controller->telemetry_starttime))
         {
             BatchedDatadogstatsd::timing('xenforo.action', microtime(true) - $controller->telemetry_starttime, 1, array('tagname' => self::$lastControllerKey));
