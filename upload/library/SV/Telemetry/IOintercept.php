41c32003041586b1b8b7a67206efe9edb26e6aea
Xon <tazipper1@hotmail.com>
2017-01-21 23:33:37 +0800

    More IO instrumentation.

--- upload/library/SV/Telemetry/IOintercept.php
+++ upload/library/SV/Telemetry/IOintercept.php
@@ -84,7 +84,15 @@ class SV_Telemetry_IOintercept
             return False;
         }
         $recursive = ($options & STREAM_MKDIR_RECURSIVE) == STREAM_MKDIR_RECURSIVE;
-        return mkdir($path, $mode, $recursive);
+        $queryTime = microtime(true);
+        try
+        {
+            return mkdir($path, $mode, $recursive);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'mkdir'));
+        }
     }
 
     public function rmdir($path, $options)
@@ -94,7 +102,15 @@ class SV_Telemetry_IOintercept
         {
             return False;
         }
-        return rmdir($path);
+        $queryTime = microtime(true);
+        try
+        {
+            return rmdir($path);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'rmdir'));
+        }
     }
 
     protected $streamhandle;
@@ -103,9 +119,17 @@ class SV_Telemetry_IOintercept
 
     public function stream_close()
     {
-        $ret = fclose($this->streamhandle);
-        unset($this->streamhandle);
-        return $ret;
+        $queryTime = microtime(true);
+        try
+        {
+            $ret = fclose($this->streamhandle);
+            unset($this->streamhandle);
+            return $ret;
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'close'));
+        }
     }
 
     public function stream_eof()
@@ -123,7 +147,15 @@ class SV_Telemetry_IOintercept
         {
             return false;
         }
-        return fflush($this->streamhandle);
+        $queryTime = microtime(true);
+        try
+        {
+            return fflush($this->streamhandle);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'flush'));
+        }
     }
 
     public function stream_metadata($path, $option, $value)
@@ -134,30 +166,38 @@ class SV_Telemetry_IOintercept
             return False;
         }
         $ret = False;
-        switch($option)
+        $queryTime = microtime(true);
+        try
         {
-            case STREAM_META_TOUCH:
-                if (isset($value[0]) && isset($value[1]))
-                    $ret = touch($path, $value[0], $value[1]);
-                else if (isset($value[0]))
-                    $ret = touch($path, $value[0]);
-                else
-                    $ret = touch($path);
-                break;
-            case STREAM_META_OWNER_NAME:
-            case STREAM_META_OWNER:
-                $ret = chown($path, $value);
-                break;
-            case STREAM_META_GROUP_NAME:
-            case STREAM_META_GROUP:
-                $ret = chgrp($path, $value);
-                break;
-            case STREAM_META_ACCESS:
-                $ret = chmod($path, $value);
-                break;
-            default:
-                throw new Exception("SV_Telemetry_IOintercept::stream_metadata ". $option ." not implemented");
-                break;
+            switch($option)
+            {
+                case STREAM_META_TOUCH:
+                    if (isset($value[0]) && isset($value[1]))
+                        $ret = touch($path, $value[0], $value[1]);
+                    else if (isset($value[0]))
+                        $ret = touch($path, $value[0]);
+                    else
+                        $ret = touch($path);
+                    break;
+                case STREAM_META_OWNER_NAME:
+                case STREAM_META_OWNER:
+                    $ret = chown($path, $value);
+                    break;
+                case STREAM_META_GROUP_NAME:
+                case STREAM_META_GROUP:
+                    $ret = chgrp($path, $value);
+                    break;
+                case STREAM_META_ACCESS:
+                    $ret = chmod($path, $value);
+                    break;
+                default:
+                    throw new Exception("SV_Telemetry_IOintercept::stream_metadata ". $option ." not implemented");
+                    break;
+            }
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'metadata'));
         }
         return $ret;
     }
@@ -169,18 +209,25 @@ class SV_Telemetry_IOintercept
         {
             throw new Exception('stream_open() passed an invalid path');
         }
-        $use_include_path  = ($options & STREAM_USE_PATH) == STREAM_USE_PATH;
-        $this->streamhandle = fopen($parsedPath, $mode, $use_include_path);
-        if ($this->streamhandle !== False)
+        $queryTime = microtime(true);
+        try
         {
-            $opened_path = $path;
+            $use_include_path  = ($options & STREAM_USE_PATH) == STREAM_USE_PATH;
+            $this->streamhandle = fopen($parsedPath, $mode, $use_include_path);
+            if ($this->streamhandle !== False)
+            {
+                $opened_path = $path;
+            }
+            else
+            {
+                throw new Exception('Call to fopen() failed');
+            }
+            return $this->streamhandle !== False;
         }
-        else
+        finally
         {
-            throw new Exception('Call to fopen() failed');
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'open'));
         }
-
-        return $this->streamhandle !== False;
     }
 
     public function stream_read($count)
@@ -189,7 +236,15 @@ class SV_Telemetry_IOintercept
         {
             return false;
         }
-        return fread($this->streamhandle,$count);
+        $queryTime = microtime(true);
+        try
+        {
+            return fread($this->streamhandle,$count);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'read'));
+        }
     }
 
     public function stream_seek($offset, $whence  = SEEK_SET)
@@ -230,7 +285,15 @@ class SV_Telemetry_IOintercept
         {
             return false;
         }
-        return ftruncate($this->streamhandle, $new_size);
+        $queryTime = microtime(true);
+        try
+        {
+            return ftruncate($this->streamhandle, $new_size);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'truncate'));
+        }
     }
 
     public function stream_write($data)
@@ -239,7 +302,15 @@ class SV_Telemetry_IOintercept
         {
             return false;
         }
-        return fwrite($this->streamhandle, $data);
+        $queryTime = microtime(true);
+        try
+        {
+            return fwrite($this->streamhandle, $data);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'write'));
+        }
     }
 
     public function unlink($path)
@@ -249,7 +320,15 @@ class SV_Telemetry_IOintercept
         {
             return False;
         }
-        return @unlink($path);
+        $queryTime = microtime(true);
+        try
+        {
+            return @unlink($path);
+        }
+        finally
+        {
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'unlink'));
+        }
     }
 
     public function url_stat($path, $flags)
@@ -259,13 +338,21 @@ class SV_Telemetry_IOintercept
         {
             return 0;
         }
-        if ($flags & STREAM_URL_STAT_LINK)
+        $queryTime = microtime(true);
+        try
         {
-            return ($flags & STREAM_URL_STAT_QUIET) ? @lstat($path) : lstat($path);
+            if ($flags & STREAM_URL_STAT_LINK)
+            {
+                return ($flags & STREAM_URL_STAT_QUIET) ? @lstat($path) : lstat($path);
+            }
+            else
+            {
+                return ($flags & STREAM_URL_STAT_QUIET) ? @stat($path) : stat($path);
+            }
         }
-        else
+        finally
         {
-            return ($flags & STREAM_URL_STAT_QUIET) ? @stat($path) : stat($path);
+            BatchedDatadogstatsd::timing('xenforo.io', microtime(true) - $queryTime, 1, array('io' => 'stat'));
         }
     }
 }
